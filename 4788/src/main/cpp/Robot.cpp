dcd7fc04ddf47e0190f61f7e796b30de963e902a
Anna <anna.pedersen@student.education.wa.edu.au>
2021-12-19 23:18:51 +0800

    linux athena problems, going to try wml as a submodule

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -1,7 +1,54 @@
 #include "Robot.h"
 
-void Robot::RobotInit() {}
+using namespace frc;
+using namespace wml;
+
+using hand = frc::XboxController::JoystickHand;
+
+double currentTimeStamp;
+double lastTimeStamp;
+double dt;
+
+double rightPower = 0;
+double leftPower = 0;
+
+double motorSpeed = 0;
+double speedIntake = 0;
+
+double constexpr deadzone = 0.05;
+//srx
+void Robot::RobotInit() {
+	driver = new frc::XboxController(0);
+	coDriver = new frc::XboxController(1);
+
+	// //drivebase 
+	_leftMotor1 = new wml::VictorSpx(3);
+	_leftMotor2 = new wml::VictorSpx(4);
+	_leftMotor3 = new wml::VictorSpx(5);
+
+	_rightMotor1 = new wml::VictorSpx(6);
+	_rightMotor2 = new wml::VictorSpx(7);
+	_rightMotor3 = new wml::VictorSpx(8);
+
+	// //intake 
+	_victorIntake = new wml::VictorSpx(1);
+	// //hammer time 
+	_hammerMotor = new wml::TalonSrx(2);
+
+	_victorIntake->SetInverted(true);
+	_hammerMotor->SetInverted(false);
+
+	_leftMotor1->SetInverted(true);
+	_leftMotor2->SetInverted(true);
+	_leftMotor3->SetInverted(true);
+
+	_rightMotor1->SetInverted(false);
+	_rightMotor2->SetInverted(false);
+	_rightMotor3->SetInverted(false);
+}
+
 void Robot::RobotPeriodic() {}
+
 
 void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
@@ -9,8 +56,58 @@ void Robot::DisabledPeriodic() {}
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
+// Manual Robot Logic
 void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+	currentTimeStamp = Timer::GetFPGATimestamp();
+	dt = currentTimeStamp - lastTimeStamp;
+
+	//its hammer time 
+	//go forwards with the left trigger 
+	motorSpeed = coDriver->GetTriggerAxis(hand::kLeftHand);
+	if (motorSpeed >= deadzone) {
+		_hammerMotor->Set(0.8);
+	} else {
+		_hammerMotor->Set(0);
+	}
+
+	//go backwards with the y button 
+	if (coDriver->GetYButton()) {
+		_hammerMotor->Set(-0.5);
+	}
+
+	//drive base, tank drive
+	if (fabs(driver->GetY(hand::kRightHand)) >= deadzone) {
+		leftPower = driver->GetY(hand::kRightHand);
+	} else {
+		leftPower = 0;
+	}
+
+	if (fabs(driver->GetY(hand::kLeftHand)) >= deadzone ) {
+		rightPower = driver->GetY(hand::kLeftHand);
+	} else {
+		rightPower = 0;
+	}
+
+	//intake, right trigger
+	speedIntake = coDriver->GetTriggerAxis(hand::kRightHand);
+	if (speedIntake >= deadzone) {
+		_victorIntake->Set(1);
+	} else {
+		_victorIntake->Set(0);
+	}
+
+	//drivebase 
+	_leftMotor1->Set(leftPower);
+	_leftMotor2->Set(leftPower);
+	_leftMotor3->Set(leftPower);
+	
+	_rightMotor1->Set(rightPower);
+	_rightMotor2->Set(rightPower);
+	_rightMotor3->Set(rightPower);
+
+	lastTimeStamp = currentTimeStamp;
+}
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
--- 4788/src/main/include/Robot.h
+++ 4788/src/main/include/Robot.h
@@ -1,10 +1,43 @@
 #pragma once
 
-#include <iostream>
-#include <frc/TimedRobot.h>
+#include <rev/CANSparkMax.h>
+#include <rev/CANEncoder.h>
 
 #include <WMLRev.h>
 #include <WMLCtre.h>
+
+#include <iostream>
+
+#include <frc/Timer.h>
+#include <frc/TimedRobot.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/Spark.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/GenericHID.h>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <networktables/NetworkTableInstance.h>
+
+#include "strategy/StrategyController.h"
+#include "strategy/MPStrategy.h"
+#include "NTProvider.h"
+#include "WMLCtre.h"
+#include "controllers/Controllers.h"
+#include "Gearbox.h"
+#include "actuators/BinaryServo.h"
+#include "actuators/Compressor.h"
+#include "actuators/DoubleSolenoid.h"
+#include "actuators/VoltageController.h"
+#include "sensors/Encoder.h"
+#include "sensors/LimitSwitch.h"
+#include "sensors/NavX.h"
+#include "sensors/PressureSensor.h"
+#include "Drivetrain.h"
+
+#include <cameraserver/CameraServer.h>
+#include <frc/DriverStation.h> 
+
+#include "Toggle.h"
 
 class Robot : public frc::TimedRobot {
  public:
@@ -22,4 +55,23 @@ class Robot : public frc::TimedRobot {
 
 	void TestInit() override;
 	void TestPeriodic() override;
+
+	// controllers
+	frc::XboxController *driver;
+	frc::XboxController *coDriver;
+
+ private:
+	//drivebase 
+	wml::VictorSpx *_leftMotor1;
+	wml::VictorSpx *_leftMotor2;
+	wml::VictorSpx *_leftMotor3;
+
+	wml::VictorSpx *_rightMotor1;
+	wml::VictorSpx *_rightMotor2;
+	wml::VictorSpx *_rightMotor3;
+
+	//intake 
+	wml::VictorSpx *_victorIntake;
+	//hammer time 
+	wml::TalonSrx *_hammerMotor;
 };
