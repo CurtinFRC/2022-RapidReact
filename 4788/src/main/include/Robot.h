ded2fc393e6a86a5d949647b0ca098aca04752b8
Connor Buchel <connorbuchel@gmail.com>
2022-01-02 21:18:21 +0800

    Added comments for Robot.h

--- 4788/src/main/include/Robot.h
+++ 4788/src/main/include/Robot.h
@@ -7,18 +7,37 @@
 
 class Robot : public frc::TimedRobot, protected wml::StrategyController, protected wml::NTProvider {
  public:
+
+	/**
+	 * Robot boot initilization.
+	 * Then robot continuous periodic (regardless of mode [teleop/auto/test etc..])
+	 */
 	void RobotInit() override;
 	void RobotPeriodic() override;
 
+	/**
+	 * In it's dissabled mode, simillar to robot init/periodic. However only executes once
+	 * robot has been disabled at least once.
+	 */
 	void DisabledInit() override;
 	void DisabledPeriodic() override;
 
+	/**
+	 * When the drivestation starts init auto mode,
+	 * these functions will execute, once for init. And then continuously for periodic
+	 */
 	void AutonomousInit() override;
 	void AutonomousPeriodic() override;
 
+	/**
+	 * Teleop version of auto, used for manual control
+	 */
 	void TeleopInit() override;
 	void TeleopPeriodic() override;
 
+	/**
+	 * Test mode. Runs only in test mode
+	 */
 	void TestInit() override;
 	void TestPeriodic() override;
 };
