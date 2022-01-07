ec8014ce28a9ba79a30603f54822c624ca2f5d24
Blake <blakechase05@gmail.com>
2022-01-07 12:37:39 +0800

    Added intake

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -1,24 +1,79 @@
 #include "Robot.h"
 
 using namespace frc;
-using namespace wml;
+// using namespace wml;
+
+const int deadzone = 0.15;
+bool pressed = false;
+
+
+// Intake Wheel Motors
+wml::TalonSrx wheelMotorL{99};
+wml::TalonSrx wheelMotorR{99};
+
+// Solenoids
+wml::actuators::DoubleSolenoid pistonL{0, 1, 0.2};
+wml::actuators::DoubleSolenoid pistonR{99, 99, 0.2};
+
+// Compressor
+wml::actuators::Compressor compressor{99};
+
+// Xbox Controller
+wml::controllers::XboxController xbox{1};
 
 void Robot::RobotInit() {}
 
-void Robot::RobotPeriodic() {}
-
+void Robot::RobotPeriodic() {
+	compressor.SetTarget(wml::actuators::kForward);
+}
 
 void Robot::DisabledInit() {
 	InterruptAll(true);
 }
+
 void Robot::DisabledPeriodic() {}
 
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
 // Manual Robot Logic
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopInit() {
+	wheelMotorL.SetInverted(true);
+	wheelMotorR.SetInverted(false);
+}
+
+void Robot::TeleopPeriodic() {
+	double wheelSpeed = 0;
+
+	if(xbox.GetButton(xbox.kX)) {
+		pressed = true;
+	}
+
+	// Intake Wheels
+	if(xbox.GetAxis(xbox.kLeftThrottle) >= deadzone) {
+		wheelSpeed += xbox.GetAxis(xbox.kRightThrottle);
+	}
+
+	// Outake Wheels
+	if(xbox.GetAxis(xbox.kRightThrottle) >= deadzone) {
+		wheelSpeed += -xbox.GetAxis(xbox.kRightThrottle);
+	}
+
+	// Intake Pistons
+	if(pressed == true) {
+		pistonL.SetTarget(wml::actuators::kForward);
+		pistonR.SetTarget(wml::actuators::kForward);
+	} 
+	
+	if(pressed == false) {
+		pistonL.SetTarget(wml::actuators::kReverse);
+		pistonR.SetTarget(wml::actuators::kReverse);
+	}
+
+	wheelMotorL.Set(wheelSpeed);
+	wheelMotorR.Set(wheelSpeed);
+
+}
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
