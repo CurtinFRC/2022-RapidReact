e447a95bd96a73c5c25d2e499ef36ad7e80d44e9
Anna <anna.pedersen@student.education.wa.edu.au>
2022-01-28 10:29:16 +0800

    pushing to main

--- 4788/src/main/cpp/Intake.cpp
+++ 4788/src/main/cpp/Intake.cpp
@@ -6,5 +6,5 @@ Intake::Intake (RobotMap::IntakeSystem &intakeSystem, Controllers &contGroup) : 
 
 void Intake::teleopOnUpdate (double dt){
   double intakeCont = fabs(_contGroup.GetController(1).Get(ControlMap::intake)) > ControlMap::triggerDeadzone ? _contGroup.GetController(1).Get(ControlMap::intake) : 0;
-  _intakeSystem.intake.Set(-intakeCont); //invert the intake motor s
+  _intakeSystem.intake.Set(-intakeCont); //invert the intake motor
 }
