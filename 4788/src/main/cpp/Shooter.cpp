b0a7d1361e9c07ac74fbbb8b0d88663f90376ce5
Anna <anna.pedersen@student.education.wa.edu.au>
2022-01-28 12:59:47 +0800

    fixed controllers

--- 4788/src/main/cpp/Shooter.cpp
+++ 4788/src/main/cpp/Shooter.cpp
@@ -7,20 +7,34 @@ using namespace wml::controllers;
 Shooter::Shooter(RobotMap::ShooterSystem &shooterSystem, SmartControllerGroup &contGroup) : _shooterSystem(shooterSystem), _contGroup(contGroup) {}
 
 void Shooter::teleopOnUpdate(double dt) {
-  double shooterSpeed = fabs(_contGroup.GetController(1).Get(ControlMap::ShooterManualSpin)) > ControlMap::triggerDeadzone ? _contGroup.GetController(1).Get(ControlMap::ShooterManualSpin) : 0;
+  // double shooterSpeed = fabs(_contGroup.GetController(1).Get(ControlMap::ShooterManualSpin)) > ControlMap::triggerDeadzone ? _contGroup.GetController(1).Get(ControlMap::ShooterManualSpin) : 0;
+  double shooterSpeed = fabs(_contGroup.Get(ControlMap::ShooterManualSpin)) > ControlMap::triggerDeadzone ? _contGroup.Get(ControlMap::ShooterManualSpin) : 0;
 
   shooterSpeed *= ControlMap::shooterMaxSpeed;
 
-  if (_contGroup.GetController(1).Get(ControlMap::ShooterSlow)) {
+  // if (_contGroup.GetController(1).Get(ControlMap::ShooterSlow)) {
+  //   ControlMap::shooterMaxSpeed = 0.25;
+  // }
+  // if (_contGroup.GetController(1).Get(ControlMap::ShooterMid)) {
+  //   ControlMap::shooterMaxSpeed = 0.5;
+  // }
+  // if (_contGroup.GetController(1).Get(ControlMap::ShooterSpeedChange)) {
+  //   ControlMap::shooterMaxSpeed = 0.75;
+  // }
+  // if (_contGroup.GetController(1).Get(ControlMap::ShooterHighSpeed)) {
+  //   ControlMap::shooterMaxSpeed = 1;
+  // }
+
+  if (_contGroup.Get(ControlMap::ShooterSlow)) {
     ControlMap::shooterMaxSpeed = 0.25;
   }
-  if (_contGroup.GetController(1).Get(ControlMap::ShooterMid)) {
+  if (_contGroup.Get(ControlMap::ShooterMid)) {
     ControlMap::shooterMaxSpeed = 0.5;
   }
-  if (_contGroup.GetController(1).Get(ControlMap::ShooterSpeedChange)) {
+  if (_contGroup.Get(ControlMap::ShooterSpeedChange)) {
     ControlMap::shooterMaxSpeed = 0.75;
   }
-  if (_contGroup.GetController(1).Get(ControlMap::ShooterHighSpeed)) {
+  if (_contGroup.Get(ControlMap::ShooterHighSpeed)) {
     ControlMap::shooterMaxSpeed = 1;
   }
 
@@ -30,6 +44,9 @@ void Shooter::teleopOnUpdate(double dt) {
   _shooterSystem.rightFlyWheelMotor.Set(shooterSpeed);
   _shooterSystem.centerFlyWheelMotor.Set(shooterSpeed);
 
-  double indexSpeed = fabs(_contGroup.GetController(1).Get(ControlMap::IndexSpin)) > ControlMap::triggerDeadzone ? _contGroup.GetController(1).Get(ControlMap::IndexSpin) : 0;
+  // double indexSpeed = fabs(_contGroup.GetController(1).Get(ControlMap::IndexSpin)) > ControlMap::triggerDeadzone ? _contGroup.GetController(1).Get(ControlMap::IndexSpin) : 0;
+  double indexSpeed = fabs(_contGroup.Get(ControlMap::IndexSpin)) > ControlMap::triggerDeadzone ? _contGroup.Get(ControlMap::IndexSpin) : 0;
   _shooterSystem.indexWheel.Set(indexSpeed);
 }
+
+//GET PEOPLE ASSEMBLING 
