13f92d743a9b34e6b11f4a47eece282a5e91383f
Viola <violablanducci@gmail.com>
2022-02-16 20:05:33 +0800

    working kinda well

--- 4788/src/main/include/Trajectories.h
+++ 4788/src/main/include/Trajectories.h
@@ -1,7 +1,8 @@
 #include "Trajectory.h"
 
 struct Trajectories {
-  ::Trajectory test{{{1,-3}, {0,0}, {1,1}, {5.5,1}}};
+  ::Trajectory test{{{1,-3}, {0,0}, {0.8,0.8}, {1.5,2}, {1,7.5}}};
+  // ::Trajectory test{{{0,0}, {3,0}, {1,-1}, {0,-3}}};
 
   void build() {
     test.build();
