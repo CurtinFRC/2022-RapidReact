7408f6313bb38ff331953b5f21aa6135296bf722
JoystickMaster-race <foxlocks179@gmail.com>
2022-03-13 13:05:03 +0800

    Added rangefinding, needs testing

--- Coproc/src/main/cpp/Vision.cpp
+++ Coproc/src/main/cpp/Vision.cpp
@@ -1,5 +1,6 @@
 #include "Vision.h" // required before other #includes
 #include "UDP_TransferNT.h"
+#include "math.h"
 //#include "ImageProcessing/Contours.h"
 
 using namespace UDP_TransferNT;
@@ -23,23 +24,20 @@ public:
 
   void onAttach() override {
     CJ_PRINT_INFO("Capture Layer Attached");
-    #ifdef COPROC
     _camera.config.port = 0;
-    #else
-    _camera.config.port = 1;
-    #endif
-    _camera.config.fps = 30;
+    // #else
+    // _camera.config.port = 1;
+    // #endif
+    _camera.config.fps = 60;
     _camera.config.autoExposure = false;
-    _camera.config.exposure = -11;
+    _camera.config.exposure = (float)0;
 
     _camera.config.name = "Example Camera";
-
+    
     _images.originImage.name = "Origin Image";
     _images.filteredImage.name = "Filtered Image";
     _images.contouredImage.name = "Contoured Image";
     _images.boundImage.name = "Bound Image";
-
-
 
     if (_camera.init() != 0) {
       CJ_PRINT_ERROR("Could not init camera");
@@ -62,24 +60,60 @@ private:
 };
 
 class FilterLayer : public CJ::Layer {
-public:
+  public:
   FilterLayer(CJ::Application &app, Images &images) : Layer("Filter Layer"), _app(app), _images(images) {
-    CJ_PRINT_INFO("Filtered Layer created");
+    CJ_PRINT_INFO("Filter Layer Created");
   }
-
   void onAttach() override {
-    //CJ::ColourFilter::createFilterTrackbar(_options);
-  }
+    CJ_PRINT_INFO("Filter Layer Attached");
+    //_images.filterImage.name = "Filtered Image";
 
+    options.HL = 0; // hue low
+    options.HH = 255; //hue highfilter
+    
+    options.SL = 0; // sat low
+    options.SH = 255; // sat high
+
+    options.VL = 0; // value low
+    options.VH = 255; // value high
+
+    options.erosionSize = 0;
+    options.dilationSize = 0;
+
+    CJ::ColourFilter::createFilterTrackbar(options); 
+  }
+  void onDetach() override {
+    CJ_PRINT_WARN("Filter Layer Detached");
+  }
   void onUpdate() override {
-    CJ::ColourFilter::filter(_images.originImage, _images.filteredImage, _options);
+    CJ::ColourFilter::filter(_images.originImage, _images.filteredImage, options); 
   }
-
-private:
-  CJ::ColourFilter::GrayScale_Options _options {8, 0, 0, 19, 255};
+  private:
   CJ::Application &_app;
   Images &_images;
+
+  CJ::ColourFilter::HSV_Options options;
 };
+// class FilterLayer : public CJ::Layer {
+// public:
+//   FilterLayer(CJ::Application &app, Images &images) : Layer("Filter Layer"), _app(app), _images(images) {
+//     CJ_PRINT_INFO("Filtered Layer created");
+//   }
+
+//   void onAttach() override {
+//     //CJ::ColourFilter::createFilterTrackbar(_options);
+//   }
+
+//   void onUpdate() override {
+//     CJ::ColourFilter::filter(_images.originImage, _images.filteredImage, _options);
+//   }
+
+// private:
+//   CJ::ColourFilter::GrayScale_Options _options {8, 0, 0, 19, 255};
+//   CJ::Application &_app;
+//   Images &_images;
+// };
+
 //void createContoursTrackbar(int &threshold);
 
 class ContoursLayer : public CJ::Layer {
@@ -120,10 +154,9 @@ public:
   void onAttach() override {
     CJ_PRINT_INFO("Bounding Layer Attached");
     
-    _network->getSocket().setIP("10.47.88.150"); // 10.47.88.2
+    _network->getSocket().setIP("10.47.88.2"); // 10.47.88.2
     _network->getSocket().setPort(5801);
     _network->init();
-
   //	_outputImg.name = "Bounding Image";
   }
 
@@ -146,6 +179,9 @@ public:
     CJ::BoundingPoints center;
     double biggestArea = 0;
     int biggestIndex = 0;
+
+    static std::vector<double> pixel  = {42.0, 94.0, 122.0, 139.0, 150.0, 157.0, 163.0, 168.0, 171.0}; 
+    static std::vector<double> cm     = {20.0, 30.0, 40.0,  50.0,  60.0,  70.0,  80.0,  90.0,  100.0};
     
     _images.boundImage.data = cv::Mat::zeros(_images.contouredImage.data.size(), CV_8UC3);
     for (size_t i = 0; i < _images.contouredImage.hull.size(); i++) {
@@ -159,70 +195,77 @@ public:
       double area = cv::contourArea(_images.contouredImage.contours[i]);
       double solidity = area / cv::contourArea(_images.contouredImage.hull[i]);
 
-       if (bufferArea > biggestArea){ //&& area > 300 && solidity < 0.75 ){
+       if (bufferArea > biggestArea && area > 300 && solidity < 0.75 ){
          center.center_x = boundRect[i].x + boundRect[i].width/2;
          center.center_y = boundRect[i].y + boundRect[i].height/2;
          biggestRect = boundRect[i];
          biggestArea = bufferArea;
          biggestIndex = i;
        }
-
-    }
+         if ((center.center_y > pixel[0]) && (center.center_x < pixel[pixel.size()-1]))
+       {
+         uint32_t i = 0;
+         while (center.center_y < pixel[i+1])
+         {
+          i++;
+         }
+      
+    double distance = cm[i] + ((center.center_y - pixel[i]) * (cm[i+1] - cm[i]) / (pixel[i+1] - pixel[i]));
     double bufferWidth = (biggestRect.width);
     double bufferHeight = (biggestRect.height);
-    
     cv::Point centerRect(center.center_x, center.center_y);
     cv::Point width(biggestRect.width);
     cv::Point height(biggestRect.height);
     //std::vector<std::vector<cv::Point>> widthRect(width.width);
-    cv::putText(_images.boundImage.data, "xy(" + std::to_string(center.center_x) + "," + std::to_string(center.center_y) + ")", (centerRect + cv::Point(-25,25)), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0,30,255));
-    cv::putText(_images.boundImage.data, "width(" + std::to_string(biggestRect.width) + ")", (centerRect + cv::Point(-50,50)), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0,30,255));
+    //cv::putText(_images.boundImage.data, "xy(" + std::to_string(center.center_x) + "," + std::to_string(center.center_y) + ")", (centerRect + cv::Point(-25,25)), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0,30,255));
     cv::putText(_images.boundImage.data, "height(" + std::to_string(biggestRect.height) + ")", (centerRect + cv::Point(25, -25)), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 30, 255));
+    cv::putText(_images.boundImage.data, "width(" + std::to_string(biggestRect.width) + ")", (centerRect + cv::Point(25, 100)), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 30, 255));
+    cv::putText(_images.boundImage.data, "Distance (cm, xy)(" + std::to_string(distance) + ")", cv::Point(center.center_x, center.center_y), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 30, 255));
     cv::circle(_images.boundImage.data, centerRect, 3, cv::Scalar(255, 0, 0));
-    cv::drawContours(_images.boundImage.data, contours_poly, biggestIndex, cv::Scalar(255,0,0));
+    cv::drawContours(_images.boundImage.data, contours_poly, biggestIndex, cv::Scalar(255, 0, 0));
     cv::rectangle(_images.boundImage.data, biggestRect.tl(), biggestRect.br(), cv::Scalar(255, 0, 0));
 
     _dpSend.setDecimals(0, center.center_x);
     _dpSend.setDecimals(1, center.center_y);
     _dpSend.setDecimals(2, _app.getDt());
-
+    _dpSend.setDecimals(3, distance);
     _network->dpSend(_dpSend);
+    }
   }
-  
+}
 private:
   CJ::Application &_app;
   Images &_images;
   CJ::BoundingPoints object_xy;
   DataPacket _dpSend;
   Network *_network;
-
 };
-
+  
 class DisplayLayer : public CJ::Layer {
 public:
   DisplayLayer(CJ::Application &app, Images &images) : Layer("Display Layer"), _app(app), _images(images) {}
 
   void onAttach() override {
     _streamServer.start();
-    // _streamServerFilter.start();
+    _streamServerFilter.start();
   }
 
   void onDetach() override {
     _streamServer.stop();
-    // _streamServerFilter.stop();
+    _streamServerFilter.stop();
   }
 
   void onUpdate() override {
-    if (CJ::Output::display(30, _images.boundImage) == 27) {
+    if (CJ::Output::display(30, _images.filteredImage, _images.boundImage, _images.contouredImage) == 27) {
       _app.setRunning(false);
     }
 
-    // _streamServerFilter.output(_images.filteredImage);
+    _streamServerFilter.output(_images.filteredImage);
     _streamServer.output(_images.originImage);
   }
 private:
   CJ::Output::Stream _streamServer{5802};
-  // CJ::Output::Stream _streamServerFilter{5803};
+  CJ::Output::Stream _streamServerFilter{5803};
   CJ::Application &_app;
   Images &_images;
 };
@@ -250,3 +293,48 @@ private:
 };
 
 CJ_CREATE_APPLICATION(VisionApplication)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
